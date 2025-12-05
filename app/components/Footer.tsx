d5c6350b3ad9a55e93eb0492ff6173d3c2e7773b
Nazim Boudeffa <boudeffa@gmail.com>
2025-12-05 20:04:06 +0100

    fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,6 @@ function Footer() {
     <footer className="bg-zinc-200 dark:bg-zinc-900 text-center p-4 mt-8">
         <p className="text-sm text-zinc-600 dark:text-zinc-400">L&apos;Auteur original est Stéphane Pellicier du site <a href="https://maphilosophie.fr" target="_blank" rel="noopener noreferrer">maphilosophie.fr</a></p>
         <p className="text-sm text-zinc-600 dark:text-zinc-400">Ce site est une adaptation non officielle et indépendante à but éducatif de code</p>
-        <p className="text-sm text-zinc-600 dark:text-zinc-400">Je vous laisse lire les textes des auteurs sur le site officiel</p>
     </footer>
 );
 }
